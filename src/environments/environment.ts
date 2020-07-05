9f0d651774a057dca5c7488eb2ee4ed3e2a9ed71
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2020-07-05 18:20:36 -0500

    Arregando Tabla

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   HOST: 'https://covid-03-beta.herokuapp.com',
   mapbox: 'pk.eyJ1IjoieGF2aW5vb2IiLCJhIjoiY2s0Mzh4bXZuMDN1NzNpcnJndXVzOXV2MiJ9.IQ7TZdyB6OnCy-8m7mqVhQ',
  
-  API: 'http://localhost:8080',
+  API: 'https://api.montecino.tech',
   TOKEN_AUTH_USERNAME: 'mitomediapp',
   TOKEN_AUTH_PASSWORD: 'mito89codex',
   TOKEN_NAME: 'access_token'
